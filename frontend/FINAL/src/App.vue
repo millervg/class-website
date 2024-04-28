f36822fb3714c6db1f417b58515dc73128f09f89
Abuchi Osuala <shsh@gmail.com>
2024-04-27 19:39:43 -0500

    Finishing off the homepage(hopefully)

--- frontend/FINAL/src/App.vue
+++ frontend/FINAL/src/App.vue
@@ -13,6 +13,15 @@ const routes = {
 
 }
 
+const socials = [
+  'mdi-instagram',
+  'mdi-linkedin',
+  'mdi-github',
+
+]
+
+
+
 const currentPath = ref(window.location.hash)
 
 window.addEventListener("hashchange", () => {
@@ -95,12 +104,97 @@ const drawer = ref(false);
       </ul>
       <v-btn href="#/home" icon="mdi-home-outline"></v-btn>
       <v-btn icon="mdi-magnify"></v-btn>
-      <!-- <v-btn icon="mdi-dots-vertical"></v-btn> -->
     </v-app-bar>
 
     <v-main style="background-color: darkslategray;">
       <component :is="currentView"></component>
     </v-main>
-    <v-footer app color="black">Copyright 2024 by FilmFinder.com</v-footer>
+
+    <v-footer color="black">
+  Copyright 2024 by FilmFinder.com
+  <div class="footer">
+    <a href="https://www.instagram.com/vickigracee/" class="social-link">
+      <v-icon>{{ socials[0] }}</v-icon>
+    </a>
+    <a href="https://www.linkedin.com/in/abuchiosuala/" class="social-link">
+      <v-icon>{{ socials[1] }}</v-icon>
+    </a>
+    <a href="https://github.com/millervg" class="social-link">
+      <v-icon>{{ socials[2] }}</v-icon>
+    </a>
+  </div>
+</v-footer>
   </v-app>
 </template>
+
+
+<style scoped>
+
+
+@import url('https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900&display=swap');
+
+
+*{
+    margin: 0;
+    padding: 0;
+    font-family: 'Poppins', sans-serif;
+
+}
+
+.btn{
+  margin-right: 10px;
+  width: auto;
+
+}
+
+.social-link{
+  padding: 5px;
+  color: white;
+}
+
+.social-link:hover{
+  color:#8a2be2;
+}
+
+
+.footer{
+  display: flex;
+  margin-left: auto;
+  align-items: center;
+
+
+}
+
+
+.home{
+  text-decoration: none;
+  color: white; 
+  text-transform: capitalize;
+
+}
+
+.navigation-list {
+  list-style-type: none;
+  margin: 0; 
+  padding: 0; 
+}
+
+.navigation-list li {
+  display: inline; 
+  margin-right: 20px; 
+}
+
+.navigation-list li a {
+  text-decoration: none; 
+  color: white; 
+  text-transform: capitalize;
+  margin-right: 30px;
+}
+
+.navigation-list li a:hover {
+  color: blueviolet;
+}
+
+
+
+</style>
